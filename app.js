66b26a7bc8c5e20d113e0e68881f1b8df4131743
play.js <git@gituser.com>
2021-01-21 10:46:49 +0100

    routing added, html start

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const path      = require('path');
 const app       = express();
 const port      = 80;
 
+// Routers
+var user = require(path.join(__dirname, '/routes/user/user.js'))
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.engine('.html', require('ejs').__express);
 app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
+
+// Routing
+app.use('/user', user);
 
 // Index site
 app.get('/', (req, res) => {
